9e43094996d7ecdda7a8c8c358ce5802de37725a
razian <contact@rznet.fr>
2022-03-08 12:51:18 +0100

    Implement ifconfig.io features

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,14 @@ FROM python:3-slim-bullseye
 
 WORKDIR /app
 
-COPY . /app
+COPY requirements.txt /app/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade pip
 RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
 VOLUME ["/var/lib/GeoIP"]
+
+COPY . /app
 
 ENV FLASK_APP=geoipweb
 CMD [ "python", "-m" , "flask", "run", "--host=0.0.0.0", "--port=80" ]
--- geoipweb.py
+++ geoipweb.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
 import geoip2.database
-import socket, requests, random
+import socket, requests, random, yaml
 from netaddr import *
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 cli = ["curl", "HTTPie", "httpie-go", "Wget", "fetch libfetch", "Go", "Go-http-client", "ddclient", "Mikrotik", "xh"]
+colors = [ '#2488bf', '#d84d3d', '#f39700', '#4caf50' ]
+
+def to_yaml(value):
+    return (yaml.dump(value, default_flow_style=False))
+app.jinja_env.filters['to_yaml'] = to_yaml
 
 #############################
 ### INFORMATIONS QUERRIES ###
@@ -15,7 +20,6 @@ cli = ["curl", "HTTPie", "httpie-go", "Wget", "fetch libfetch", "Go", "Go-http-c
 #
 # Get IP's host
 #
-
 def gethost(ip):
   try:
     name = socket.getnameinfo((ip, 0), 0)[0]
@@ -25,9 +29,28 @@ def gethost(ip):
   return (name)
 
 #
+# Get request headers
+#
+def gethead(request, ip):
+  ip = request.headers.get('X-Client-Ip')
+  head = {
+    "ip_address": ip,
+    "remote_host": gethost(ip),
+    "port": request.environ.get('REMOTE_PORT'),
+    "user-agent": request.headers.get("User-Agent"),
+    "mime_type": request.headers.get("Accept"),
+    "language": request.headers.get("Accept-Language"),
+    "encoding": request.headers.get("Accept-Encoding"),
+    "method": request.method,
+    "cache-control": request.headers.get("Cache-Control"),
+    "x-forwarded-for": request.headers.get("X-Forwarded-For").split(',')[0],
+    "x-forwarded-proto": request.headers.get("X-Forwarded-Proto")
+  }
+  return (head)
+
+#
 # Location GeoIP database informations querry
 #
-
 def getcity(ip):
   geoipath = "/var/lib/GeoIP"
   try:
@@ -41,7 +64,6 @@ def getcity(ip):
 #
 # ASN GeoIP database informations querry
 #
-
 def getasn(ip):
   geoipath = "/var/lib/GeoIP"
   try:
@@ -55,11 +77,10 @@ def getasn(ip):
 #
 # Network informations querry
 #
-
 def getnet(ip):
   try:
     network = requests.get(f"https://internetdb.shodan.io/{ip}").json()
-    network = dict([(k.capitalize(),v) for k,v in network.items() if len(v)>0])
+    network = dict([(k.lower(),v) for k,v in network.items() if len(v)>0])
   except:
     print(f"No net data about {ip}")
     network = "No net data about {ip}"
@@ -68,21 +89,20 @@ def getnet(ip):
 #
 # GeoIP informations put in a dict
 #
-
 def getgeo(ip):
   infos = {
-    "IP Address": ip
+    "ip_address": ip
   }
-  infos.update({"Hostname": gethost(ip)})
+  infos.update({"hostname": gethost(ip)})
 
   city_res = getcity(ip)
   if city_res:
     infos.update({
-        "Country": city_res.country.name,
-        "Country Code": city_res.country.iso_code,
-        "City": city_res.city.name,
-        "Latitude": city_res.location.latitude,
-        "Longitude": city_res.location.longitude
+        "country": city_res.country.name,
+        "country_code": city_res.country.iso_code,
+        "city": city_res.city.name,
+        "latitude": city_res.location.latitude,
+        "longitude": city_res.location.longitude
     })
   else:
     return (f"No location data about {ip}")
@@ -90,13 +110,39 @@ def getgeo(ip):
   asn_res = getasn(ip)
   if asn_res:
     infos.update({
-      "ISP": asn_res.autonomous_system_organization,
-      "ASN": f"AS{asn_res.autonomous_system_number}"
+      "isp": asn_res.autonomous_system_organization,
+      "asn": f"AS{asn_res.autonomous_system_number}"
     })
   else:
     return (f"No ISP data about {ip}")
 
   return (infos)
+
+#
+# Return a single info
+#
+def getself(request, info):
+  ip = request.headers.get('X-Client-Ip')
+  infos = gethead(request, ip)
+  infos.update(getgeo(ip))
+  infos.update(getnet(ip))
+
+  info = info.lower()
+
+  if info == "all":
+    return (to_yaml(infos))
+  if info == "json":
+    return (infos)
+
+  if not info in infos and info != "all":
+    return (f"No data about {info} for {ip}")
+
+  if isinstance(infos[info], list):
+    conv = { i : infos[info][i] for i in range(0, len(infos[info]) ) }
+    infos[info] = conv
+    return (to_yaml(infos[info]))
+
+  return (infos[info] + '\n')
 
 ####################
 ### FLASK ROUTES ###
@@ -105,7 +151,6 @@ def getgeo(ip):
 #
 # Specific info path
 #
-
 @app.route('/<ip>/<info>')
 def getspec(ip, info):
   if IPAddress(ip).is_private():
@@ -114,12 +159,12 @@ def getspec(ip, info):
   infos = getgeo(ip)
   infos.update(getnet(ip))
 
-  if len(info) == 3:
-    info = info.upper()
-  elif info == "cc" or info == "countryiso":
-    info = "Country Code"
-  else:
-    info = info.capitalize()
+  info = info.lower()
+
+  if info == "all":
+    return (to_yaml(infos))
+  if info == "json":
+    return (infos)
 
   if not info in infos:
     return (f"No data about {info} for {ip}")
@@ -127,13 +172,39 @@ def getspec(ip, info):
   if isinstance(infos[info], list):
     conv = { i : infos[info][i] for i in range(0, len(infos[info]) ) }
     infos[info] = conv
+    return (to_yaml(infos[info]))
 
   return (infos[info] + '\n')
 
 #
+# IP path
+#
+@app.route('/<ip>')
+def getinfo(ip):
+  try:
+    IPAddress(ip)
+  except:
+    return (getself(request, ip))
+  if IPAddress(ip).is_private():
+    return (f"{ip} is a private IP\n")
+
+  infos = getgeo(ip)
+  network = getnet(ip)
+
+  if any(x in request.headers.get('User-Agent') for x in cli):
+    infos.update(network)
+    return (to_yaml(infos))
+  else:
+    return (render_template('template.html',
+            hostname=request.headers.get("host"),
+            ip=request.headers.get('X-Client-Ip'),
+            infos=infos,
+            network=network,
+            color=random.choice(colors)))
+
+#
 # Json path
 #
-
 @app.route('/<ip>/json')
 def getinfojson(ip):
   if IPAddress(ip).is_private():
@@ -145,33 +216,21 @@ def getinfojson(ip):
   return (infos)
 
 #
-# IP path
-#
-
-@app.route('/<ip>')
-def getinfo(ip):
-  if IPAddress(ip).is_private():
-    return (f"{ip} is a private IP\n")
-
-  infos = getgeo(ip)
-  network = getnet(ip)
-  colors = [ '#2488bf', '#d84d3d', '#f39700', '#4caf50' ]
-
-  if any(x in request.headers.get('User-Agent') for x in cli):
-    infos.update(network)
-    return (infos), 404
-  else:
-    return (render_template('template.html', ip=ip, infos=infos, network=network, color=random.choice(colors)))
-
-#
 # Root path
 #
-
 @app.route('/')
-@app.route('/self')
 def self():
-  ip = request.environ.get('HTTP_X_FORWARDED_FOR').split(',')
+  ip = request.headers.get('X-Client-Ip')
   if any(x in request.headers.get('User-Agent') for x in cli):
-    return (ip[0] + '\n'), 404
-  else:
-    return (getinfo(ip[0]))
+    return (ip + '\n')
+  head = gethead(request, ip)
+  infos = getgeo(ip)
+  network = getnet(ip)
+
+  return (render_template('template.html',
+          hostname=request.headers.get("host"),
+          ip=ip,
+          head=head,
+          infos=infos,
+          network=network,
+          color=random.choice(colors)))
